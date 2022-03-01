a67448e919d47a3ec03768caf2421e707e7c721c
charlesb <bernardcharles@gmail.com>
2022-03-01 14:49:39 +0800

    datastore MI subscription_id

--- src/register_adlsgen2_as_datastore.py
+++ src/register_adlsgen2_as_datastore.py
@@ -5,7 +5,7 @@ from azureml.core import Datastore
 import aml_utils
 
 
-def main(datastore_name, account_name, filesystem):
+def main(datastore_name, account_name, filesystem, subscription_id):
 
     ws = aml_utils.retrieve_workspace()
 
@@ -14,6 +14,7 @@ def main(datastore_name, account_name, filesystem):
         datastore_name=datastore_name,
         account_name=account_name,
         filesystem=filesystem,
+        subscription_id=subscription_id,
         grant_workspace_access=True
     ) 
 
@@ -25,6 +26,7 @@ def parse_args(args_list=None):
     parser.add_argument('--datastorename', type=str, required=True)
     parser.add_argument('--accountname', type=str, required=True)
     parser.add_argument('--filesystem', type=str, required=True)
+    parser.add_argument('--subscriptionid', type=str, required=True)
     args_parsed = parser.parse_args(args_list)
     return args_parsed
 
@@ -35,5 +37,6 @@ if __name__ == "__main__":
     main(
         datastore_name=args.datastorename,
         account_name=args.accountname,
-        filesystem=args.filesystem
+        filesystem=args.filesystem,
+        subscription_id=args.subscriptionid
     )
