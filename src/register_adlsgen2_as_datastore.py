c68f8501f97c060ed5cf42a550bdf67bd16caabf
charlesb <bernardcharles@gmail.com>
2022-03-01 12:06:30 +0800

    remove dash from args name

--- src/register_adlsgen2_as_datastore.py
+++ src/register_adlsgen2_as_datastore.py
@@ -21,8 +21,8 @@ def main(datastore_name, account_name, filesystem):
 
 def parse_args(args_list=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datastore-name', type=str, required=True)
-    parser.add_argument('--account-name', type=str, required=True)
+    parser.add_argument('--datastorename', type=str, required=True)
+    parser.add_argument('--accountname', type=str, required=True)
     parser.add_argument('--filesystem', type=str, required=True)
     args_parsed = parser.parse_args(args_list)
     return args_parsed
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     args = parse_args()
 
     main(
-        datastore_name=args.datastore-name,
-        account_name=args.account-name,
+        datastore_name=args.datastorename,
+        account_name=args.accountname,
         filesystem=args.filesystem
     )
